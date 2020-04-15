ee615f99a009af6b7d0635a13271c2f479063d7b
SandroMota1977 <storm.mota@hotmail.com>
2020-04-15 18:23:30 -0300

    Very BAD solution.

---
+++ src/application/Program.java
@@ -0,0 +1,44 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import model.entities.Reservation;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.print("Nº do Quarto: ");
+		int numQuarto = sc.nextInt();
+		System.out.print("Check-in data (dd/MM/yyyy): ");
+		Date checkIn = sdf.parse(sc.next());
+		System.out.print("Check-out data (dd/MM/yyyy): ");
+		Date checkOut = sdf.parse(sc.next());
+		
+		if (! checkOut.after(checkIn)) {
+			System.out.println("Erro na reserva! a data de check-out antecede a data de check-in.");
+		}
+		else {
+			Reservation reservation = new Reservation(numQuarto, checkIn, checkOut);
+			System.out.println("Reserva: " + reservation);
+			
+			System.out.println();
+			System.out.print("Insira os dados para atualizar a reserva: ");
+			System.out.print("Check-in data (dd/MM/yyyy): ");
+			checkIn = sdf.parse(sc.next());
+			System.out.print("Check-out data (dd/MM/yyyy): ");
+			checkOut = sdf.parse(sc.next());
+			
+			reservation.updateDates(checkIn, checkOut);
+			System.out.println("Reserva: " + reservation);
+		}
+	}
+
+}
+;
